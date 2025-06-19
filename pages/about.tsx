cc4583ddf435bb8372bc91a3fb175ec7951ed580
Ola Gaarden <ola@tempely.com>
2025-06-20 00:38:56 +0200

    Some update

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-gray-50 to-white px-6 py-16 text-gray-800 font-sans">
       <div className="max-w-3xl mx-auto text-center">
-        <h1 className="text-5xl font-bold mb-6 text-blue-700 tracking-tight">About Templa</h1>
+        <h1 className="text-5xl font-bold mb-6 text-blue-700 tracking-tight">About Tempely</h1>
         <p className="text-lg mb-6 text-gray-700">
         Helping creators grow smarter with digital templates that actually work.
         </p>
