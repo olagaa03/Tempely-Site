e3156bf396a06d5f789f5753669cbba62f84336e
Ola Gaarden <ola@tempely.com>
2025-06-25 22:19:38 +0200

    Some update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import { ClerkProvider } from '@clerk/nextjs';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import '@/styles/globals.css';
-console.log('Clerk Frontend API:', process.env.NEXT_PUBLIC_CLERK_FRONTEND_API);
+
+console.log('Clerk Publishable Key:', process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+console.log('Env Vars:', {
+  clerkPublishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  anyOtherVar: process.env.NEXT_PUBLIC_TEMP_PASSWORD, // just example
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <div className="min-h-screen bg-white text-black flex flex-col">
         <Header />
         <main className="flex-grow">
