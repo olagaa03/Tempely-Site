6b600b553d61d4632e700442fd34f3881fac4537
Ola Gaarden <olagaa03@gmail.com>
2025-07-11 23:01:42 +0200

    Some update

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -16,9 +16,11 @@ export default function Topbar() {
   useEffect(() => {
     if (darkMode) {
       document.documentElement.setAttribute('data-theme', 'dark');
+      document.documentElement.classList.add('dark');
       localStorage.setItem('theme', 'dark');
     } else {
       document.documentElement.setAttribute('data-theme', 'light');
+      document.documentElement.classList.remove('dark');
       localStorage.setItem('theme', 'light');
     }
   }, [darkMode]);
